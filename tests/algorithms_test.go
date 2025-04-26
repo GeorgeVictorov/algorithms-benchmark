2fb345ad9846140fae4da8e7665a5a7907391b26
georgiiviktorov <nowayisleft@gmail.com>
2025-04-26 17:40:02 +0400

    feat(tests): refactor

--- tests/algorithms_test.go
+++ tests/algorithms_test.go
@@ -6,19 +6,24 @@ import (
 	"testing"
 )
 
-func TestBubbleSort(t *testing.T) {
-	cases := []struct {
-		input    []int
-		expected []int
-	}{
-		{input: []int{3, 2, 1}, expected: []int{1, 2, 3}},
-		{input: []int{1, 2, 3}, expected: []int{1, 2, 3}},
-		{input: []int{5, -1, 3}, expected: []int{-1, 3, 5}},
-		{input: []int{}, expected: []int{}},
-	}
+var testCases = []struct {
+    input    []int
+    expected []int
+}{
+    {input: []int{3, 2, 1}, expected: []int{1, 2, 3}},
+    {input: []int{1, 2, 3}, expected: []int{1, 2, 3}},
+    {input: []int{5, -1, 3}, expected: []int{-1, 3, 5}},
+    {input: []int{}, expected: []int{}},
+    {input: []int{0, 0, 0}, expected: []int{0, 0, 0}},
+}
 
-	for _, c := range cases {
-		result := algorithm.BubbleSort(c.input)
+
+func TestBubbleSort(t *testing.T) {
+	for _, c := range testCases {
+        inputCopy := make([]int, len(c.input))
+        copy(inputCopy, c.input)
+
+		result := algorithm.BubbleSort(inputCopy)
 		if !reflect.DeepEqual(result, c.expected) {
 			t.Errorf("BubbleSort(%v) == %v, expected %v", c.input, result, c.expected)
 		}
@@ -26,18 +31,11 @@ func TestBubbleSort(t *testing.T) {
 }
 
 func TestBubbleSortFlag(t *testing.T) {
-	cases := []struct {
-		input    []int
-		expected []int
-	}{
-		{input: []int{3, 2, 1}, expected: []int{1, 2, 3}},
-		{input: []int{1, 2, 3}, expected: []int{1, 2, 3}},
-		{input: []int{5, -1, 3}, expected: []int{-1, 3, 5}},
-		{input: []int{}, expected: []int{}},
-	}
+	for _, c := range testCases {
+        inputCopy := make([]int, len(c.input))
+        copy(inputCopy, c.input)
 
-	for _, c := range cases {
-		result := algorithm.BubbleSortFlag(c.input)
+		result := algorithm.BubbleSortFlag(inputCopy)
 		if !reflect.DeepEqual(result, c.expected) {
 			t.Errorf("BubbleSortFlag(%v) == %v, expected %v", c.input, result, c.expected)
 		}
