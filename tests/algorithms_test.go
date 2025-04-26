decbddc293975329b429eee4d81f478bf31f7a8f
georgiiviktorov <nowayisleft@gmail.com>
2025-04-26 17:33:50 +0400

    feat(tests): bubble sort flag test

--- tests/algorithms_test.go
+++ tests/algorithms_test.go
@@ -1,27 +1,45 @@
 package tests
 
 import (
-    "reflect"
-    "testing"
-    "github.com/GeorgeVictorov/algorithms-benchmark/algorithm"
+	"github.com/GeorgeVictorov/algorithms-benchmark/algorithm"
+	"reflect"
+	"testing"
 )
 
 func TestBubbleSort(t *testing.T) {
-    cases := []struct {
-        input []int
-        expected []int
-    }{
-        {input: []int{3, 2, 1}, expected: []int{1, 2, 3}},
-        {input: []int{1, 2, 3}, expected: []int{1, 2, 3}},
-        {input: []int{5, -1, 3}, expected: []int{-1, 3, 5}},
-        {input: []int{}, expected: []int{}},
-    }
+	cases := []struct {
+		input    []int
+		expected []int
+	}{
+		{input: []int{3, 2, 1}, expected: []int{1, 2, 3}},
+		{input: []int{1, 2, 3}, expected: []int{1, 2, 3}},
+		{input: []int{5, -1, 3}, expected: []int{-1, 3, 5}},
+		{input: []int{}, expected: []int{}},
+	}
 
-    for _, c := range cases {
-        result := algorithm.BubbleSort(c.input)
-        if !reflect.DeepEqual(result, c.expected) {
-            t.Errorf("BubbleSort(%v) == %v, expected %v", c.input, result, c.expected)
-        }
-    }
+	for _, c := range cases {
+		result := algorithm.BubbleSort(c.input)
+		if !reflect.DeepEqual(result, c.expected) {
+			t.Errorf("BubbleSort(%v) == %v, expected %v", c.input, result, c.expected)
+		}
+	}
 }
 
+func TestBubbleSortFlag(t *testing.T) {
+	cases := []struct {
+		input    []int
+		expected []int
+	}{
+		{input: []int{3, 2, 1}, expected: []int{1, 2, 3}},
+		{input: []int{1, 2, 3}, expected: []int{1, 2, 3}},
+		{input: []int{5, -1, 3}, expected: []int{-1, 3, 5}},
+		{input: []int{}, expected: []int{}},
+	}
+
+	for _, c := range cases {
+		result := algorithm.BubbleSortFlag(c.input)
+		if !reflect.DeepEqual(result, c.expected) {
+			t.Errorf("BubbleSortFlag(%v) == %v, expected %v", c.input, result, c.expected)
+		}
+	}
+}
