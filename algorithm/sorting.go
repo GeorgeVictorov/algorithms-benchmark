529a864bf57d9bc43a5fa3b077b88bfb0cb8f456
georgiiviktorov <nowayisleft@gmail.com>
2025-04-26 23:00:15 +0400

    feat(algorithm): add selection sort

--- algorithm/sorting.go
+++ algorithm/sorting.go
@@ -29,3 +29,17 @@ func BubbleSortFlag(arr []int) []int {
     return arr
 }
 
+func SelectionSort(arr []int) []int {
+    n := len(arr)
+    for i := 0; i < n - 1; i++ {
+        minIdx := i
+        for j := i + 1; j < n; j++ {
+            if arr[j] < arr[minIdx] {
+                minIdx = j
+            }
+        }
+        arr[i], arr[minIdx] = arr[minIdx], arr[i]
+    }
+    return arr
+}
+        
