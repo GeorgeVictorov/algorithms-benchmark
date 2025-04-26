3766b5398cfb9645cd06de85c675c083db697bbc
georgiiviktorov <nowayisleft@gmail.com>
2025-04-26 19:56:24 +0400

    feat(util): add benchmark

--- util/benchmark.go
+++ util/benchmark.go
@@ -1,31 +1,30 @@
 package util
 
 import (
-    "fmt"
-    "time"
-    "github.com/GeorgeVictorov/algorithms-benchmark/util"
+	"fmt"
+	"time"
 )
 
 type SortAlgorithm struct {
-    Name string
-    Func func([]int) [] int
+	Name string
+	Func func([]int) []int
 }
 
-func BechmarkSorts(sorts []SortAlgorithm, sizes []int) {
-    fmt.Printf("%-20s %-10s %-10s\n", "Algorithm", "Size", "Time")
+func BenchmarkSorts(sorts []SortAlgorithm, sizes []int) {
+	fmt.Printf("%-20s %-10s %-10s\n", "Algorithm", "Size", "Time")
 
-    for _, size := range sizes {
-        arr := RandomArray(size, 10000)
-    
-        for _, sort := range sorts {
-            copiedArr := make([]int, len(arr))
-            copy(copiedArr, arr)
+	for _, size := range sizes {
+		arr := RandomArray(size, 10000)
 
-            start := time.Now()
-            sort.Func(copiedArr)
-            end := time.Since(start)
+		for _, sort := range sorts {
+			copiedArr := make([]int, len(arr))
+			copy(copiedArr, arr)
 
-            fmt.Printf("%-20s %-10d %-10v\n", sort.Name, size, end)
-        }
-    }
+			start := time.Now()
+			sort.Func(copiedArr)
+			end := time.Since(start)
+
+			fmt.Printf("%-20s %-10d %-10v\n", sort.Name, size, end)
+		}
+	}
 }
