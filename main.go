c194e337154151b3a3d63271046c55cab43a878a
georgiiviktorov <nowayisleft@gmail.com>
2025-04-26 23:02:47 +0400

    feat(main): add selection sort

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ func main() {
     sorts := []util.SortAlgorithm{
 		{Name: "BubbleSort", Func: algorithm.BubbleSort},
 		{Name: "BubbleSortFlag", Func: algorithm.BubbleSortFlag},
+        {Name: "SelectionSort", Func: algorithm.SelectionSort},
     }
 
     arrSizes := []int{100, 1000, 10000}
