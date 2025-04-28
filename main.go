583d822c38ee3657c3b53cc74b9786601a9a35f4
georgiiviktorov <nowayisleft@gmail.com>
2025-04-28 12:25:36 +0400

    chore: gofmt

--- main.go
+++ main.go
@@ -10,15 +10,15 @@ func main() {
 		{Name: "BubbleSort", Func: algorithm.BubbleSort},
 		{Name: "BubbleSortFlag", Func: algorithm.BubbleSortFlag},
 		{Name: "SelectionSort", Func: algorithm.SelectionSort},
-        {Name: "InsertionSort", Func: algorithm.InsertionSort},
-        {Name: "ShellSort", Func: algorithm.ShellSort},
-        {Name: "QuickSort", Func: algorithm.QuickSort},
-        {Name: "QuickSortInplace", Func: algorithm.QuickSortInplace},
+		{Name: "InsertionSort", Func: algorithm.InsertionSort},
+		{Name: "ShellSort", Func: algorithm.ShellSort},
+		{Name: "QuickSort", Func: algorithm.QuickSort},
+		{Name: "QuickSortInplace", Func: algorithm.QuickSortInplace},
 	}
 
 	arrSizes := []int{100, 1000, 10000}
-    
-    generator := &util.RandomArrayGenerator{}
+
+	generator := &util.RandomArrayGenerator{}
 
 	util.BenchmarkSorts(sorts, arrSizes, 10, generator)
 }
--- util/benchmark.go
+++ util/benchmark.go
@@ -19,53 +19,54 @@ type BenchmarkResult struct {
 }
 
 type ArrayGenerator interface {
-    Generate(size, maxValue int) []int
+	Generate(size, maxValue int) []int
 }
 
 type RandomArrayGenerator struct{}
+
 func (r *RandomArrayGenerator) Generate(size, maxValue int) []int {
-    return RandomArray(size, maxValue)
+	return RandomArray(size, maxValue)
 }
 
 func BenchmarkSorts(sorts []SortAlgorithm, sizes []int, repeats int, generator ArrayGenerator) {
-    benchmarkStart := time.Now()
+	benchmarkStart := time.Now()
 
-    for _, size := range sizes {
-        arr := generator.Generate(size, 10000)
-        results := benchmarkForSize(sorts, arr, size, repeats)
-        printResults(size, repeats, results)
-    }
+	for _, size := range sizes {
+		arr := generator.Generate(size, 10000)
+		results := benchmarkForSize(sorts, arr, size, repeats)
+		printResults(size, repeats, results)
+	}
 
-    benchmarkDuration := time.Since(benchmarkStart)
-    fmt.Printf("\nBenchmark completed in: %v\n", benchmarkDuration)
+	benchmarkDuration := time.Since(benchmarkStart)
+	fmt.Printf("\nBenchmark completed in: %v\n", benchmarkDuration)
 }
 
 func benchmarkForSize(sorts []SortAlgorithm, arr []int, size, repeats int) []BenchmarkResult {
-    results := make([]BenchmarkResult, len(sorts))
-    var wg sync.WaitGroup
-    wg.Add(len(sorts))
+	results := make([]BenchmarkResult, len(sorts))
+	var wg sync.WaitGroup
+	wg.Add(len(sorts))
 
-    for i, sortAlg := range sorts {
-        go runBenchmark(i, sortAlg, arr, repeats, results, &wg, size)
-    }
+	for i, sortAlg := range sorts {
+		go runBenchmark(i, sortAlg, arr, repeats, results, &wg, size)
+	}
 
-    wg.Wait()
+	wg.Wait()
 
-    sort.Slice(results, func(i, j int) bool {
-        return results[i].AvgTime < results[j].AvgTime
-    })
+	sort.Slice(results, func(i, j int) bool {
+		return results[i].AvgTime < results[j].AvgTime
+	})
 
-    return results
+	return results
 }
 
 func runBenchmark(i int, sortAlg SortAlgorithm, arr []int, repeats int, results []BenchmarkResult, wg *sync.WaitGroup, size int) {
-    defer wg.Done()
-    avgTime := benchmarkSort(sortAlg, arr, repeats)
-    results[i] = BenchmarkResult{
-        Name:    sortAlg.Name,
-        Size:    size,
-        AvgTime: avgTime,
-    }
+	defer wg.Done()
+	avgTime := benchmarkSort(sortAlg, arr, repeats)
+	results[i] = BenchmarkResult{
+		Name:    sortAlg.Name,
+		Size:    size,
+		AvgTime: avgTime,
+	}
 }
 
 func benchmarkSort(sortAlg SortAlgorithm, arr []int, repeats int) time.Duration {
@@ -84,10 +85,10 @@ func benchmarkSort(sortAlg SortAlgorithm, arr []int, repeats int) time.Duration 
 }
 
 func printResults(size, repeats int, results []BenchmarkResult) {
-    fmt.Printf("\n=== Size: %d (repeats: %d) ===\n", size, repeats)
-    fmt.Printf("%-20s %-10s %-15s\n", "Algorithm", "Size", "Avg Time")
+	fmt.Printf("\n=== Size: %d (repeats: %d) ===\n", size, repeats)
+	fmt.Printf("%-20s %-10s %-15s\n", "Algorithm", "Size", "Avg Time")
 
-    for _, res := range results {
-        fmt.Printf("%-20s %-10d %-15v\n", res.Name, res.Size, res.AvgTime)
-    }
+	for _, res := range results {
+		fmt.Printf("%-20s %-10d %-15v\n", res.Name, res.Size, res.AvgTime)
+	}
 }
